8f6046af88c1d8fc5c9e55c7df2cd87ceba702a8
Philip <philip8u@gmail.com>
2025-01-01 22:44:34 +0200

    Initial Commit

---
+++ Streakly/StreaklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreaklyApp.swift
+//  Streakly
+//
+//  Created by Philip on 01.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct StreaklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
